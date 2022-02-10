e304d490c51b05c6681d9c1538b6170ed08eb40b
lonelyotter <57280827+lonelyotter@users.noreply.github.com>
2022-02-10 15:13:16 +0800

    finish lab01

---
+++ lab/lab01/lab01.py
@@ -0,0 +1,70 @@
+def falling(n, k):
+    """Compute the falling factorial of n to depth k.
+
+    >>> falling(6, 3)  # 6 * 5 * 4
+    120
+    >>> falling(4, 3)  # 4 * 3 * 2
+    24
+    >>> falling(4, 1)  # 4
+    4
+    >>> falling(4, 0)
+    1
+    """
+    result = 1
+
+    while k > 0:
+        result = result * n
+        k = k - 1
+        n = n - 1
+
+    return result
+
+
+def sum_digits(y):
+    """Sum all the digits of y.
+
+    >>> sum_digits(10) # 1 + 0 = 1
+    1
+    >>> sum_digits(4224) # 4 + 2 + 2 + 4 = 12
+    12
+    >>> sum_digits(1234567890)
+    45
+    >>> a = sum_digits(123) # make sure that you are using return rather than print
+    >>> a
+    6
+    """
+    result = 0
+
+    while y > 0:
+        result = result + (y % 10)
+        y = y // 10
+
+    return result
+
+
+def double_eights(n):
+    """Return true if n has two eights in a row.
+    >>> double_eights(8)
+    False
+    >>> double_eights(88)
+    True
+    >>> double_eights(2882)
+    True
+    >>> double_eights(880088)
+    True
+    >>> double_eights(12345)
+    False
+    >>> double_eights(80808080)
+    False
+    """
+    n_low = 0
+    n_high = 0
+
+    while n > 0:
+        n_low = n_high
+        n_high = n % 10
+        n = n // 10
+        if n_low == 8 and n_high == 8:
+            return True
+
+    return False
